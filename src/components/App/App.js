6ff752ca15df4224f8a91ebe4a870135a31aa37e
maciekgrzybek <maciekgrzybek1@gmail.com>
2021-05-11 21:37:03 +0100

    refactor: change app to functional components + remove redundant locale props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DataTable from '../DataTable/DataTable';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container mt-3">
-        <DataTable rows={this.props.rows} locale="da" rowsPerPage={5} />
-      </div>
-    );
-  }
-}
+const App = ({ rows }) => {
+  return (
+    <div className="container mt-3">
+      <DataTable rows={rows} rowsPerPage={5} />
+    </div>
+  );
+};
 
 export default App;
