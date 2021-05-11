4e9fe38c917e8b82866388f4901aaf14297d58da
maciekgrzybek <maciekgrzybek1@gmail.com>
2021-05-11 21:40:29 +0100

    refactor: change var to const in index.js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-var userData = JSON.parse(document.getElementById('user-data').dataset.users);
+const userData = JSON.parse(document.getElementById('user-data').dataset.users);
 ReactDOM.render(<App rows={userData} />, document.getElementById('root'));
